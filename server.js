1d5bdbb06902aef0e9f712b085452fa3483db8df
EricYoo955 <ericyoo@ad.unc.edu>
2021-12-02 22:28:01 -0500

    videoPush

--- server.js
+++ server.js
@@ -6,25 +6,28 @@ var db = require('./database.js')
 // Require md5 MODULE
 var md5 = require("md5");
 // Make Express use its own built-in body parser
-var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Set server port
 var HTTP_PORT = 5000
 // Start server
-app.listen(HTTP_PORT, () => {
+const server = app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {                         
-    res.json({"message":"Your API is working!"});
+    res.json({"message":"Your API works! (200)"});
 		res.status(200);
 });
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-
+app.post("/app/new/", (req, res) => {
+	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
+	const info = stmt.run(req.body.user, md5(req.body.pass));
+	res.status(201).json({"message": info.changes + " record created: ID " + info.lastInsertRowid});
+});
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
@@ -32,6 +35,7 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+// req.params.id
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
@@ -42,3 +46,10 @@ app.use(function(req, res){
 	res.json({"message":"Endpoint not found. (404)"});
     res.status(404);
 });
+
+// Tell STDOUT that the server is stopped
+process.on('SIGTERM', () => {
+	server.close(() => {
+		console.log('Server stopped.')
+	})
+})
--- test.js
+++ test.js
@@ -1,7 +1,17 @@
 var db = require("./database.js")
 var md5 = require('md5')
 
-const all = db.prepare('SELECT * FROM userinfo');
+all = db.prepare('SELECT * FROM userinfo').all();
 
-console.log("\nAll records in my database:");
+function updateall() {
+    all = db.prepare('SELECT * FROM userinfo').all();
+}
+
+console.log("\nAll records in my database:", db.name);
 console.log(all)
+
+const getuser = db.prepare('SELECT * FROM userinfo WHERE id  = ?');
+const output = getuser.get('1')
+
+console.log("\nFull record for username", output.user)
+console.log(output)
